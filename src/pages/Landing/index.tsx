e0e5cbf40eb798cc58d481b86f787c189d54673f
Guilherme <guilherme1067@hotmail.com>
2020-08-12 01:09:40 -0300

    added effect on images css

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -8,6 +8,11 @@ import noRoleModelz from '../../assets/images/J-Cole-No-Role-Models.jpg'
 import Kendrick from '../../assets/images/kendrickDamm.jpg'
 import rihannaAnti from '../../assets/images/rihanna-anti.jpg'
 import theCarters from '../../assets/images/the-carters.jpg'
+import sidoka from '../../assets/images/sidokaLanguage.jpg'
+import youngThug from '../../assets/images/young-thug.jpg'
+import vintageCulture from '../../assets/images/vintage-culture.jpg'
+import vintageCultureAutumn from '../../assets/images/vintageAutumn.jpg'
+
 
 
 import './styles.css'
@@ -20,7 +25,7 @@ function Landing(){
             </h1>
 
         <section className="albums">
-         
+
             <div>
                 <img src={racionais} alt="racionais"/>
             </div>
@@ -44,6 +49,19 @@ function Landing(){
             <div>
                 <img src={scorpion} alt="Scorpion"/>
             </div>
+            <div>
+                <img src={sidoka} alt="Scorpion"/>
+            </div>
+            <div>
+                <img src={vintageCultureAutumn} alt="Scorpion"/>
+            </div>
+            <div>
+                <img src={youngThug} alt="Scorpion"/>
+            </div>
+            <div>
+                <img src={vintageCulture} alt="Scorpion"/>
+            </div>
+            
         </section>      
         </div>
     )
